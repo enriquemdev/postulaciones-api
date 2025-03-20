f6ebc13be5f20d7c474f5f86979f7566fa5635fa
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-03-19 21:25:19 -0600

    fix(availabilities): Fix typo in Availability model and migration

--- database/migrations/2025_03_20_004908_create_availabilities_table.php
+++ database/migrations/2025_03_20_004908_create_availabilities_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('avalabilities', function (Blueprint $table) {
+        Schema::create('availabilities', function (Blueprint $table) {
             $table->id();
             $table->string('availability_name');
             $table->string('availability_code');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('avalabilities');
+        Schema::dropIfExists('availabilities');
     }
 };
