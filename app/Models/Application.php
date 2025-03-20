a703e88865ba7b2a2e2e98e738283457e5a3730c
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-03-19 21:22:44 -0600

    feat(applications): Add first relation method to Application model for ApplicationStatus relation

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
-    /** @use HasFactory<\Database\Factories\ApplicationFactory> */
-    use HasFactory;
+    protected $table = 'applications';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function applicationStatus(): BelongsTo {
+        return $this->belongsTo(ApplicationStatus::class, 'application_status_id');
+    }
 }
