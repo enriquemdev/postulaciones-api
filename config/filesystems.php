6e0badd9b193d544410db1c36ef19e970036883c
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-03-20 00:08:59 -0600

    chore(files): Create custom disk to store applicants' cvs

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -58,6 +58,14 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+        ],
+
+        // Custom disk to only store applicants' cvs
+        'cvs' => [
+            'driver' => 'local',
+            'root' => storage_path('app/cvs'),
+            'url' => env('APP_URL') . '/storage/cvs',
+            'visibility' => 'private',
         ],
 
     ],
