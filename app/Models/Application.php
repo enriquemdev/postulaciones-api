50a2daea1f8503089157579766459f4070eec4ce
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-03-19 21:42:48 -0600

    feat(applications): Update the Application model file

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Application extends Model
 {
@@ -11,7 +12,31 @@ class Application extends Model
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    protected $casts = [
+        'monthly_expected_salary' => 'float',
+    ];
+
     public function applicationStatus(): BelongsTo {
         return $this->belongsTo(ApplicationStatus::class, 'application_status_id');
+    }
+
+    public function availability(): BelongsTo {
+        return $this->belongsTo(Availability::class, 'availability_id');
+    }
+
+    public function employmentType(): BelongsTo {
+        return $this->belongsTo(EmploymentType::class, 'employment_type_id');
+    }
+
+    public function workModality(): BelongsTo {
+        return $this->belongsTo(WorkModality::class, 'work_modality_id');
+    }
+
+    public function educations(): HasMany {
+        return $this->hasMany(Education::class, 'application_id');
+    }
+
+    public function experiences(): HasMany {
+        return $this->hasMany(Experience::class, 'application_id');
     }
 }
